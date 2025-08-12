ef4432d6d4c8a3f63138dbbb98911de9da0c3f1b
AiOfLautech <babalolahephzibah2@gmail.com>
2025-08-12 00:02:20 -0500

    Delete frontend directory

--- frontend/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-// Theme toggle
-const toggle = document.getElementById('toggleTheme');
-toggle.addEventListener('click', ()=>{
-  document.body.dataset.theme = document.body.dataset.theme === 'light' ? 'dark' : 'light';
-});
-// ParticlesJS init
-particlesJS.load('particles-js','../../frontend/assets/js/particles.json');
