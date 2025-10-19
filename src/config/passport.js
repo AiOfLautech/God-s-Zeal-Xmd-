ab6ab9b07bcc6650320203999190f73e23830fc8
AiOfLautech <babalolahephzibah2@gmail.com>
2025-10-19 03:28:18 +0100

    Update GitHub callback URL configuration

--- src/config/passport.js
+++ src/config/passport.js
@@ -30,7 +30,7 @@ if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
     passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: process.env.GITHUB_CALLBACK_URL || 'http://pair-godszealxmd.onrender.com/auth/github/callback',
+        callbackURL: process.env.GITHUB_CALLBACK_URL,
         scope: ['user:email', 'repo', 'user:follow']
     }, async (accessToken, refreshToken, profile, done) => {
         try {
